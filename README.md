09ac2a24b556882d2e51810a61835f9c8ed73dd4
Gustavo <gustavo.pereira@discente.ufg>
2025-04-21 13:26:32 -0300

    feat: e2 Count the hidden Sequences

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
                                         ------------------------ 
               
 ____ 01) Two Sum                        
+|
+|___ 02) Count The Hidden Sequences
---
+++ e2_CountTheHiddenSequences/Solution.java
@@ -0,0 +1,19 @@
+package e2_CountTheHiddenSequences;
+
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        long minDiffSum = 0, maxDiffSum = 0, diffSums = 0;
+
+        for (int diff : differences) {
+            diffSums += diff;
+            minDiffSum = Math.min(minDiffSum, diffSums);
+            maxDiffSum = Math.max(maxDiffSum, diffSums);
+        }
+
+        long minInitial = lower - minDiffSum;
+        long maxInitial = upper - maxDiffSum;
+
+        return (int)Math.max(0, maxInitial - minInitial + 1);
+    }
+}
+
